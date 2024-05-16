e22702c69c1ff0859a50b82d06d924eef87904fd
Nikhil Prasad <nprasad2077@gmail.com>
2024-05-16 00:32:39 -0500

    table

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import App from './App.jsx'
 import './index.css'
 import { ApolloProvider } from '@apollo/client'
 import client from './components/apolloClient.jsx'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ApolloProvider client={client}>
