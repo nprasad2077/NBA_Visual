dbfa6e5e73c279214fc05d5aad0a961011a2a058
Nikhil Prasad <nprasad2077@gmail.com>
2024-05-28 19:37:34 -0500

    new navbar + links

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,19 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Navigation() {
   return (
-    <nav>
-        <ul class='flex'>
-            <li class='px-4'>
-                <Link to='/'>Home</Link>
-            </li>
-            <li class='px-4'>
-                <Link to='/reactvtable'>React_V_Table</Link>
-            </li>
+    <div className="navbar bg-base-100">
+      <div className="flex-1">
+        <Link to="/" className="btn btn-ghost text-xl">Home</Link>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <Link to="/reactvtable">React_V_Table</Link>
+          </li>
+          {/* <li>
+            <details>
+              <summary>Parent</summary>
+              <ul className="p-2 bg-base-100 rounded-t-none">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/reactvtable">React_V_Table</Link>
+                </li>
+              </ul>
+            </details>
+          </li> */}
         </ul>
-    </nav>
-  )
+      </div>
+    </div>
+  );
 }
 
-export default Navigation
+export default Navigation;
